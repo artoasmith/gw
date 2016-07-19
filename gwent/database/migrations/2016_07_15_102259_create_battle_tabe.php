016cf9e0107ff62561955926474dad6dd0bd64ee
Alex Hotsaliyk <artoasmith@gmail.com>
2016-07-19 10:16:03 +0300

    config

--- gwent/database/migrations/2016_07_15_102259_create_battle_tabe.php
+++ gwent/database/migrations/2016_07_15_102259_create_battle_tabe.php
@@ -14,14 +14,12 @@ class CreateBattleTabe extends Migration
     {
         Schema::create('tbl_battles', function (Blueprint $table) {
             $table->increments('id')->unsigned();
-            $table->integer('player_1_id')->unsigned();
-            $table->integer('player_2_id')->unsigned();
-            $table->text('player_1_deck'); //массив array[отбой => [], рука => [], колода => [] ]
-            $table->text('player_2_deck');
+            $table->integer('creator_id')->unsigned();
+            $table->text('players_decks'); //массив array[отбой => [], рука => [], колода => [] ]
             $table->integer('deck_weight')->unsigned();
             $table->string('league');
             $table->tinyInteger('fight_status')->unsigned(); //0 - ожидание другого игрока. 1 - подготовка к бою. 2 - бой продолжается. 3 - бой окончен
-            $table->tinyInteger('player_id_turn')->unsigned();
+            $table->tinyInteger('player_num_turn')->unsigned();
             $table->text('round_status'); //массив array[p1 => 'количество выграных раундов', p2 => 'количество выграных раундов']
             $table->text('fight_log');
             $table->timestamps();
